8b422b8e8210b0a9b1a9486de8d67484cb9b5d5d
valivalter <valivalter@gmail.com>
2026-02-05 13:45:06 +0100

    Change mail options interface

--- src/actions/mail.ts
+++ src/actions/mail.ts
@@ -5,7 +5,14 @@ import { SendMailOptions } from 'nodemailer';
 
 const resend = new Resend(process.env.EMAIL_API_KEY);
 
-export async function sendEmail(options: SendMailOptions) {
+interface EmailOptions {
+  to: string | string[];
+  subject: string;
+  html: string;
+  replyTo?: string;
+}
+
+export async function sendEmail(options: EmailOptions) {
   try {
     await resend.emails.send({
       from: `${process.env.EMAIL_FROM_NAME} <${process.env.EMAIL_FROM_ADDRESS}>`,
