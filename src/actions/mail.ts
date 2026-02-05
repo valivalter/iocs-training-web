7408e9baac0c2d1a6b8e47d33075d8517e4e51af
valivalter <valivalter@gmail.com>
2026-02-05 13:51:56 +0100

    Remove nodemailer

--- src/actions/mail.ts
+++ src/actions/mail.ts
@@ -1,18 +1,17 @@
 'use server';
 
 import { Resend } from 'resend';
-import { SendMailOptions } from 'nodemailer';
 
 const resend = new Resend(process.env.EMAIL_API_KEY);
 
-interface EmailOptions {
+interface SendMailOptions {
   to: string | string[];
   subject: string;
   html: string;
   replyTo?: string;
 }
 
-export async function sendEmail(options: EmailOptions) {
+export async function sendEmail(options: SendMailOptions) {
   try {
     await resend.emails.send({
       from: `${process.env.EMAIL_FROM_NAME} <${process.env.EMAIL_FROM_ADDRESS}>`,
