84df46dc4a6de180b53319dd830beb32aec09fde
valivalter <valivalter@gmail.com>
2026-02-05 01:38:15 +0100

    Switch mailer to Resend

--- src/actions/mail.ts
+++ src/actions/mail.ts
@@ -1,27 +1,18 @@
 'use server';
 
-import axios from 'axios';
+import { Resend } from 'resend';
 import { SendMailOptions } from 'nodemailer';
 
-const mailingAxios = axios.create({
-  baseURL: process.env.EMAIL_HOST,
-  headers: {
-    Authorization: `Api-Key ${process.env.EMAIL_API_KEY}`,
-  },
-});
+const resend = new Resend(process.env.EMAIL_API_KEY);
 
 export async function sendEmail(options: SendMailOptions) {
   try {
-    await mailingAxios.post('/api/send', {
-      from: {
-        name: process.env.EMAIL_FROM_NAME,
-        email: process.env.EMAIL_FROM_ADDRESS,
-      },
-      to: options.to,
+    await resend.emails.send({
+      from: `${process.env.EMAIL_FROM_NAME} <${process.env.EMAIL_FROM_ADDRESS}>`,
+      to: Array.isArray(options.to) ? options.to : [options.to],
       subject: options.subject,
       html: options.html,
-      replyTo: process.env.EMAIL_FROM_ADDRESS,
-      queue: 'send',
+      replyTo: options.replyTo || process.env.EMAIL_FROM_ADDRESS,
     });
   } catch (error) {
     console.error(`Failed to send email to ${options.to}`, error);
