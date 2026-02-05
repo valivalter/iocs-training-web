16b156c3710c71e0b1778ef417d751beff8287b7
valivalter <valivalter@gmail.com>
2026-02-05 12:58:01 +0100

    Add temporary carousel images

--- src/components/sections/IntroductionSection.tsx
+++ src/components/sections/IntroductionSection.tsx
@@ -6,19 +6,10 @@ import VerticalCarousel from '@/components/ui/VerticalCarousel';
 const IntroductionSection = () => {
   const text = useTranslations('IntroductionSection');
   const images = [
-    { src: '/images/groups/Bausz.png', width: 4032, height: 3024 },
-    { src: '/images/groups/Csengu.png', width: 940, height: 788 },
-    { src: '/images/groups/Della.jpg', width: 2224, height: 1668 },
-    { src: '/images/groups/Macsi.png', width: 4032, height: 3024 },
-    { src: '/images/groups/Orban.png', width: 2000, height: 1414 },
-    { src: '/images/groups/Orsi.png', width: 2000, height: 1545 },
-    { src: '/images/groups/Podonyi.jpg', width: 4032, height: 3024 },
-    { src: '/images/groups/Reka.jpg', width: 2048, height: 1448 },
-    { src: '/images/groups/Sara.jpg', width: 1425, height: 1080 },
-    { src: '/images/groups/Tami.jpg', width: 1642, height: 900 },
-    { src: '/images/groups/Tege.jpg', width: 2048, height: 1536 },
-    { src: '/images/groups/Vozi.png', width: 1640, height: 924 },
-    { src: '/images/groups/Zsombor.jpg', width: 1242, height: 702 },
+    { src: '/images/groups/1.jpg', width: 1080, height: 908 },
+    { src: '/images/groups/2.jpg', width: 1020, height: 676 },
+    { src: '/images/groups/3.jpg', width: 1080, height: 1086 },
+    { src: '/images/groups/4.jpg', width: 1006, height: 1342 },
   ];
 
   return (
