187bf38f9b93fec703cd4236f9c4329b392d7d90
valivalter <valivalter@gmail.com>
2026-02-09 06:43:21 +0100

    Update gallery images

--- src/components/sections/GallerySection.tsx
+++ src/components/sections/GallerySection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import SwipeCarousel from '@/components/ui/Gallery';
 
 const GallerySection = () => {
-  const images = Array.from({ length: 12 }, (_, index) => `/images/gallery/image-${index + 1}.jpg`);
+  const images = Array.from({ length: 11 }, (_, index) => `/images/gallery/image-${index + 1}.jpg`);
 
   return (
     <section>
